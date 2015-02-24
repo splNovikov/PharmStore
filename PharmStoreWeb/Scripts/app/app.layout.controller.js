35441dd72264623fa630ee2d322dc442e562b472
Pavel <spl.novikov@gmail.com>
2015-02-24 21:22:16 +0300

    removed app name from the rootscope

--- PharmStoreWeb/Scripts/app/app.layout.controller.js
+++ PharmStoreWeb/Scripts/app/app.layout.controller.js
@@ -3,11 +3,11 @@
 
 	angular
 		.module('pharmStore')
-		.controller('LayoutCtrl', ['$rootScope', 'APP_MAIN_CONSTANTS', LayoutCtrl])
+		.controller('LayoutCtrl', ['$scope', 'APP_MAIN_CONSTANTS', LayoutCtrl])
 
-	function LayoutCtrl($rootScope, APP_MAIN_CONSTANTS) {
+	function LayoutCtrl($scope, APP_MAIN_CONSTANTS) {
 
-		$rootScope.appTitle = APP_MAIN_CONSTANTS.appTitle;
+		$scope.appTitle = APP_MAIN_CONSTANTS.appTitle;
 
 	}
 
